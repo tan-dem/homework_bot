023789f5a8062d209f9cc0fe1db8dcc2a80659dc
Tatiana Minenko <ta.minenko@ya.ru>
2022-09-04 10:11:59 +0300

    small fixes

--- homework.py
+++ homework.py
@@ -27,7 +27,7 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-RETRY_TIME = 600
+RETRY_TIME = 120
 
 
 HOMEWORK_STATUSES = {
@@ -118,7 +118,7 @@ def check_tokens():
 def main():
     """Основная логика работы бота."""
     latest_homework_status = {}
-    latest_error_message = ''
+    latest_error_message = 'Нет домашних работ за выбранный период'
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
